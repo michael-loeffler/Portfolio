9dbeca6d742749e47ea1ae52eca0e49cfa9baf4d
mloeff <michaelloeffler23@gmail.com>
2023-04-22 21:11:58 -0500

    adds required portions to Resume page to form skeleton

--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -4,6 +4,19 @@ export default function Resume() {
   return (
     <div>
       <h1>Resume Page</h1>
+      <a href="https://docs.google.com/document/d/1WPSw44IoKUFXcExWGt3zNqWq6DeWBifn/edit" download="filename"><button className="button button-secondary">Download my Resume</button></a>
+      <h3>Front-End Proficiencies</h3>
+      <ul>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+      </ul>
+      <h3>Back-End Proficiencies</h3>
+      <ul>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+      </ul>
     </div>
   );
 }
