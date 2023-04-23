76ce82643af3f387dd996daf8957eb71af5e99f2
mloeff <michaelloeffler23@gmail.com>
2023-04-22 21:37:49 -0500

    adds starter code for footer icons to Footer.js need to read documentation on using FA in react

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
-const styles = {
-  footer: {
-      display: 'flex',
-      justifyContent: 'flex-center',
-    },
-}
-
 export default function Footer() {
   return (
-    <footer>
-      <h5 style={styles.footer}>Footer</h5>
+     <footer className='fixed-bottom d-flex justify-content-center'>
+      <div className="container pt-4">
+        <section className="mb-4">
+        <a
+          class="btn btn-link btn-floating btn-lg text-dark m-1"
+          href="#!"
+          role="button"
+          data-mdb-ripple-color="dark"
+          ><i class="fab fa-instagram"></i>
+        </a>
+        <a
+          class="btn btn-link btn-floating btn-lg text-dark m-1"
+          href="#!"
+          role="button"
+          data-mdb-ripple-color="dark"
+          ><i class="fab fa-linkedin"></i>
+        </a>
+        <a
+          class="btn btn-link btn-floating btn-lg text-dark m-1"
+          href="#!"
+          role="button"
+          data-mdb-ripple-color="dark"
+          ><i class="fab fa-github"></i>
+        </a>
+        </section>
+      </div>
     </footer>
-  );
+  )
 }
