59b7bbbb394c76c612c76b94639deb96dd29dad5
mloeff <michaelloeffler23@gmail.com>
2023-04-22 20:44:19 -0500

    adds requirements to About Me page

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import headshot from '../../assets/Loeffler, Michael - Headshot small.jpg'
 
 export default function About() {
   return (
     <div>
       <h1>About Me Page</h1>
+      <img src={headshot} alt=""/>
+      <p>Bio goes here</p>
     </div>
   );
 }
