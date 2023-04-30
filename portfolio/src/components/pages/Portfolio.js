7b44a27b4faedbacec392991a0386fb3b5c5cf67
mloeff <michaelloeffler23@gmail.com>
2023-04-30 16:36:03 -0500

    retools porfolio page to map over the new projects array of objects, creating a Project component for each project object in the project array - instead of having 6 Project separate components

--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -4,37 +4,54 @@ import thumbnail from '../../assets/placeholder-300x200.gif';
 
 export default function Portfolio() {
 
-  // consider creating an object for each project then use map over the projects array to render each project
-
-  const github = [
-    'https://github.com/michael-loeffler/Shoe-Store',
-    'https://github.com/michael-loeffler/Tech-Blog',
-    'https://github.com/michael-loeffler/Social-Network-API',
-    'https://github.com/michael-loeffler/README-Generator',
-    'https://github.com/michael-loeffler/Weather-Dashboard',
-    'https://github.com/michael-loeffler/Password-Generator'
+  const projects = [
+    {
+      title: "Full-Stack E-commerce Site",
+      src: thumbnail,
+      github: 'https://github.com/michael-loeffler/Shoe-Store',
+      deployed: 'https://mighty-brushlands-95444.herokuapp.com/'
+    },
+    {
+      title: "Tech Blog - Model View Controller framework",
+      src: thumbnail,
+      github: 'https://github.com/michael-loeffler/Tech-Blog',
+      deployed: 'https://shrouded-gorge-55801.herokuapp.com/'
+    },
+    {
+      title: "Social Network API - NoSQL/MongoDB",
+      src: thumbnail,
+      github: 'https://github.com/michael-loeffler/Social-Network-API',
+      deployed: 'https://drive.google.com/file/d/1NLW6204mswgokpV0Pn1CKKZ3e0Gx9fMp/view'
+    },
+    {
+      title: "README Generator - Node",
+      src: thumbnail,
+      github: 'https://github.com/michael-loeffler/README-Generator',
+      deployed: 'https://drive.google.com/file/d/17rqFUdYIMLsRvTYWKeA0RGSfGleac7Ae/view'
+    },
+    {
+      title: "Weather Dashboard - Server-Side APIs",
+      src: thumbnail,
+      github: 'https://github.com/michael-loeffler/Weather-Dashboard',
+      deployed: 'https://michael-loeffler.github.io/Weather-Dashboard/'
+    },
+    {
+      title: "Password Generator - JavaScript",
+      src: thumbnail,
+      github: 'https://github.com/michael-loeffler/Password-Generator',
+      deployed: 'https://michael-loeffler.github.io/Password-Generator/'
+    },
   ];
-  
-  const deployed = [
-    'https://mighty-brushlands-95444.herokuapp.com/',
-    'https://shrouded-gorge-55801.herokuapp.com/',
-    'https://drive.google.com/file/d/1NLW6204mswgokpV0Pn1CKKZ3e0Gx9fMp/view',
-    'https://drive.google.com/file/d/17rqFUdYIMLsRvTYWKeA0RGSfGleac7Ae/view',
-    'https://michael-loeffler.github.io/Weather-Dashboard/',
-    'https://michael-loeffler.github.io/Password-Generator/'
-  ];
-
 
   return (
-    <div style={{paddingBottom: "50px"}}>
+    <div style={{ paddingBottom: "50px" }}>
       <h1 className='text-center mt-3'>Portfolio</h1>
       <div className="card-deck d-flex flex-wrap justify-content-around">
-        <Project title="Full-Stack E-commerce Site" src={thumbnail} github={github[0]} deployed={deployed[0]}/> 
-        <Project title="Tech Blog - MVC" src={thumbnail} github={github[1]} deployed={deployed[1]}/>
-        <Project title="Social Network API - NoSQL/MongoDB" src={thumbnail} github={github[2]} deployed={deployed[2]}/>
-        <Project title="README Generator - Node" src={thumbnail} github={github[3]} deployed={deployed[3]}/>
-        <Project title="Weather Dashboard - server side APIs" src={thumbnail} github={github[4]} deployed={deployed[4]}/>
-        <Project title="Password Generator - JavaScript" src={thumbnail} github={github[5]} deployed={deployed[5]}/>
+        {projects.map((project) => {
+          return (
+            <Project title={project.title} src={project.src} github={project.github} deployed={project.deployed} />
+          );
+        })}
       </div>
     </div>
   );
