1ecb2a96a048929d7ad52226b27099acd1dc32cf
mloeff <michaelloeffler23@gmail.com>
2023-04-30 15:21:16 -0500

    retools Contact form to use React-Bootstrap elements also adds form validation onSubmit and onBlur need to refine onBlur to happen only on specific field you click off of

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,40 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 export default function Contact() {
+  const [validated, setValidated] = useState(false);
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   form.checkValidity();
-  // }
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    setValidated(true);
+  };
 
   return (
     <div style={{paddingBottom: "50px"}} className='d-flex flex-column align-items-center'>
       <h1 className='mt-2'>Contact Page</h1>
 
-      <form className="needs-validation col-md-6" noValidate>
-      <div className="form-group">
-        <label for="name">Name</label>
-        <input type="text" className="form-control" id="name" placeholder="Kevin McCallister" required/>
-        <div className="invalid-feedback">
+      <Form className="needs-validation col-md-6" noValidate validated={validated} onSubmit={handleSubmit} onBlur={handleSubmit}>
+      <Form.Group>
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Kevin McCallister" required/>
+        <Form.Control.Feedback type='invalid'>
           This field is required.
-        </div>
-      </div>  
-      <div className="form-group">
-        <label for="email">Email address</label>
-        <input type="email" className="form-control" id="email" placeholder="name@example.com" required/>
-        <div className="invalid-feedback">
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" required/>
+        <Form.Control.Feedback type='invalid'>
+          Valid Email Address is required.
+        </Form.Control.Feedback>
+      </Form.Group> 
+      <Form.Group>
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows={3} required></Form.Control>
+        <Form.Control.Feedback type='invalid'>
           This field is required.
-        </div>
-      </div>
-      <div className="form-group">
-        <label for="message">Message</label>
-        <textarea className="form-control" id="message" rows="3" required></textarea>
-        <div className="invalid-feedback">
-          This field is required.
-        </div>
-      </div>
-      <button type="submit" className="btn btn-primary mt-2">Send</button>
-      </form>
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Button type="submit" className="btn btn-primary mt-2">Send</Button>
+      </Form>
     </div>
   );
 }
