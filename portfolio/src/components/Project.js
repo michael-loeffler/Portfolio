fd8fbba7578e65f31d97ee9a8d5d58be463cf33e
mloeff <michaelloeffler23@gmail.com>
2023-04-30 16:36:59 -0500

    destructures props to eliminate the need to include props. before each prop

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../App.css';
 
-function Project(props) {
+function Project({title, src, github, deployed}) {
     return (
     <div className="card w-25 mb-5 mt-3 mx-1">
-        <img className="card-img-top" src={props.src} alt="" />
+        <img className="card-img-top" src={src} alt="" />
         <div className="card-body">
-            <h5 className="card-title">{props.title}<span className="badge badge-pill badge-dark">Node</span></h5>
+            <h5 className="card-title">{title}<span className="badge badge-pill badge-dark">Node</span></h5>
             <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
         <div className="card-footer d-flex justify-content-between">
-            <a href={props.deployed} target="_blank" rel="noopener noreferrer"><small className="text-muted">Deployed App</small></a>
-            <a href={props.github}><small className="text-muted">GitHub Repo</small></a>
+            <a href={deployed} target="_blank" rel="noopener noreferrer"><small className="text-muted">Deployed App</small></a>
+            <a href={github} target="_blank" rel="noopener noreferrer"><small className="text-muted">GitHub Repo</small></a>
         </div>
     </div>
     )
