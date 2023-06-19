355a70ffc652eebd83f3215903563bd300d9ad2c
mloeff <michaelloeffler23@gmail.com>
2023-06-19 16:11:01 -0500

    Updates

---
+++ precache-manifest.249b437873e997cf6acad8c5cae99e72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "613c9e638e771ab694d9",
+    "url": "/Portfolio/static/css/main.d416656a.chunk.css"
+  },
+  {
+    "revision": "613c9e638e771ab694d9",
+    "url": "/Portfolio/static/js/main.613c9e63.chunk.js"
+  },
+  {
+    "revision": "1e07bca912b568a6eec9",
+    "url": "/Portfolio/static/css/1.52fcc520.chunk.css"
+  },
+  {
+    "revision": "1e07bca912b568a6eec9",
+    "url": "/Portfolio/static/js/1.1e07bca9.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "e0eca5755ccd1523c2b024df756ecb72",
+    "url": "/Portfolio/static/media/Loeffler, Michael - Headshot small.e0eca575.jpg"
+  },
+  {
+    "revision": "032349da5da57bf56468d44ecd130f94",
+    "url": "/Portfolio/static/media/ecommerce.032349da.PNG"
+  },
+  {
+    "revision": "1f414c582cd7df3318afe02405e7c99f",
+    "url": "/Portfolio/static/media/tech.1f414c58.PNG"
+  },
+  {
+    "revision": "e70ce9d00c0157be9587a48dbff1514f",
+    "url": "/Portfolio/static/media/social.e70ce9d0.PNG"
+  },
+  {
+    "revision": "f051488254d050249bd10ded29d6e84f",
+    "url": "/Portfolio/static/media/readme.f0514882.PNG"
+  },
+  {
+    "revision": "b5bc652083c55fbf9c0ef51872e10b8b",
+    "url": "/Portfolio/static/media/weather.b5bc6520.PNG"
+  },
+  {
+    "revision": "cc0d52cbfec5784d0abdf277c029fa7b",
+    "url": "/Portfolio/static/media/passGen.cc0d52cb.PNG"
+  },
+  {
+    "revision": "08490861f018f56d64458e13e9874500",
+    "url": "/Portfolio/static/media/Loeffler, Michael - Resume.08490861.pdf"
+  },
+  {
+    "revision": "c06397ece38def1608c2230d05feda46",
+    "url": "/Portfolio/index.html"
+  }
+];
