3b28f01a0a974b23498263d107b1e5133c8253cb
mloeff <michaelloeffler23@gmail.com>
2023-06-20 21:10:00 -0500

    adds functionality to prepopulate an email draft when user clicks submit on Contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,7 +4,7 @@ import { Form, Button } from 'react-bootstrap';
 export default function Contact() {
   const [validated, setValidated] = useState(false);
 
-  const handleSubmit = (e) => {
+  const validateForm = (e) => {
     const form = e.currentTarget;
     if (form.checkValidity() === false) {
       e.preventDefault();
@@ -13,28 +13,31 @@ export default function Contact() {
     setValidated(true);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const name = e.target[0].value;
+    const message = e.target[1].value;
+    if (name !== "" && message !== "") {
+      document.location = "mailto:michaelloeffler23@gmail.com?subject="+name+" wants to connect!&body="+message;
+    }
+  };
+
   return (
     <div style={{paddingBottom: "50px"}} className='d-flex flex-column align-items-center'>
       <h1 className='text-center mt-3'>Contact Me</h1>
 
-      <Form className="needs-validation col-md-6" noValidate validated={validated} onSubmit={handleSubmit} onBlur={handleSubmit}>
+      <Form className="needs-validation col-md-6" noValidate validated={validated} onSubmit={handleSubmit} onBlur={validateForm}>
       <Form.Group>
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Peter Parker" required/>
+        <Form.Control type="text" name="name" placeholder="Peter Parker" required/>
         <Form.Control.Feedback type='invalid'>
           This field is required.
         </Form.Control.Feedback>
       </Form.Group>
       <Form.Group>
-        <Form.Label>Email Address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" required/>
-        <Form.Control.Feedback type='invalid'>
-          Valid Email Address is required.
-        </Form.Control.Feedback>
-      </Form.Group> 
-      <Form.Group>
         <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" rows={3} required></Form.Control>
+        <Form.Control as="textarea" name="message" rows={3} required></Form.Control>
         <Form.Control.Feedback type='invalid'>
           This field is required.
         </Form.Control.Feedback>
