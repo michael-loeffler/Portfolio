c058f81d639b709797aa15cbe7fd0a5957059749
mloeff <michaelloeffler23@gmail.com>
2023-06-20 20:10:07 -0500

    uploads new copy of resume and rearranges competencies

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import resume from '../../assets/Loeffler, Michael - Resume.pdf'
+import resume from '../../assets/Resume - Michael Loeffler.pdf'
 
 export default function Resume() {
   return (
     <div className="d-flex flex-column align-items-center" style={{paddingBottom: "50px"}}>
       <h1 className='text-center mt-3'>Resume</h1>
-      <a href={resume} download="Loeffler, Michael - Resume.pdf" target='_blank' rel='noopener noreferrer'><button className="btn btn-info mt-3">Download my Resume</button></a>
+      <a href={resume} download="Resume - Michael Loeffler.pdf" target='_blank' rel='noopener noreferrer'><button className="btn btn-info mt-3">Download my Resume</button></a>
 
         <div className="card mt-4 text-center w-25">
           <div className="card-header">
             <h5 className="mb-0">Front-End Proficiencies</h5>
           </div>
             <ul className="list-group">
-              <li className="list-group-item">HTML</li>
+              <li className="list-group-item">HTML5</li>
               <li className="list-group-item">CSS</li>
               <li className="list-group-item">Bootstrap</li>
               <li className="list-group-item">JavaScript</li>
               <li className="list-group-item">jQuery</li>
+              <li className="list-group-item">Handlebars</li>
+              <li className="list-group-item">React</li>
             </ul>
         </div>
      
@@ -28,13 +30,11 @@ export default function Resume() {
               <li className="list-group-item">Node</li>
               <li className="list-group-item">Object-Oriented Programming</li>
               <li className="list-group-item">Express</li>
-              <li className="list-group-item">SQL</li>
+              <li className="list-group-item">MySQL</li>
               <li className="list-group-item">Sequelize</li>
-              <li className="list-group-item">Handlebars</li>
               <li className="list-group-item">Model View Controller framework</li>
               <li className="list-group-item">MongoDB</li>
               <li className="list-group-item">Mongoose</li>
-              <li className="list-group-item">React</li>
               <li className="list-group-item">MERN framework (MongoDB, Express, React, Node)</li>
             </ul>
         </div>
