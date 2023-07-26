550a502629697ddc894e32d8ea2e3fe4ff8c2445
mloeff <michaelloeffler23@gmail.com>
2023-07-25 22:04:05 -0500

    implements mobile-first design for Contact page-- visually pleasing at all screen sizes

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
+import '../../style.css';
 
 export default function Contact() {
   const [validated, setValidated] = useState(false);
@@ -24,25 +25,25 @@ export default function Contact() {
   };
 
   return (
-    <div style={{paddingBottom: "50px"}} className='d-flex flex-column align-items-center'>
+    <div className='d-flex flex-column align-items-center'>
       <h1 className='text-center mt-3'>Contact Me</h1>
 
-      <Form className="needs-validation col-md-6" noValidate validated={validated} onSubmit={handleSubmit} onBlur={validateForm}>
-      <Form.Group>
+      <Form className="needs-validation" id='form' noValidate validated={validated} onSubmit={handleSubmit} onBlur={validateForm}>
+      <Form.Group className='mt-2'>
         <Form.Label>Name</Form.Label>
         <Form.Control type="text" name="name" placeholder="Peter Parker" required/>
         <Form.Control.Feedback type='invalid'>
           This field is required.
         </Form.Control.Feedback>
       </Form.Group>
-      <Form.Group>
+      <Form.Group className='mt-2'>
         <Form.Label>Message</Form.Label>
         <Form.Control as="textarea" name="message" rows={3} required></Form.Control>
         <Form.Control.Feedback type='invalid'>
           This field is required.
         </Form.Control.Feedback>
       </Form.Group>
-      <Button type="submit" className="btn btn-primary mt-2">Send</Button>
+      <Button type="submit" className="btn btn-primary mt-3">Send</Button>
       </Form>
     </div>
   );
