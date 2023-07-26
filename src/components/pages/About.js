e7651bddf2ef5d3c6c71c5867fdd85e1778741a6
mloeff <michaelloeffler23@gmail.com>
2023-07-25 23:13:49 -0500

    implements mobile-first design for About Me page-- improves appearance on all screen sizes still needs tweaking, also attempted to fix visual bug on mobile version of resume page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import headshot from '../../assets/Loeffler, Michael - Headshot small.jpg'
+import "../../style.css";
 
 export default function About() {
   return (
-    <div style={{paddingBottom: "50px"}}>
+    <div>
       <h1 className='text-center mt-3'>About Me</h1>
-      <div className="mt-4 d-flex justify-content-around align-items-top mh-50">
-        <img className='col-md-3' src={headshot} alt=""/>
-          <p className='ml-3 mb-0 col-md-7 lead'>
+      <div id='about' className="mt-4">
+        <img id='headshot' src={headshot} alt="" />
+        <p id='bio' className='lead'>
           Motivated Full-Stack Web Developer and recent graduate of the Coding Boot Camp at Northwestern University. Background in Engineering and Higher Education Administration, pursuing a career shift into the tech world. Skilled problem-solver and fast learner who is passionate about gaining new competencies and working through challenges with increased efficiency. Strengths include collaboration, attention to detail, flexibility, innovation, project management, and clear communication.
-          </p>
+        </p>
       </div>
     </div>
   );
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,7 +8,7 @@ export default function Resume() {
       <h1 className='text-center mt-3'>Resume</h1>
       <a href={resume} download="Resume - Michael Loeffler.pdf" target='_blank' rel='noopener noreferrer' className="btn btn-outline-info mt-3" id='resume'>Download my Resume</a>
 
-        <div className="card mt-4 text-center">
+        <div id='skills' className="card mt-4 text-center">
           <div className="card-header">
             <h5 className="mb-0">Front-End Proficiencies</h5>
           </div>
