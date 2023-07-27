907c406cc5df0a8f5741e8d9a9eb1d28e6d6ce9b
mloeff <michaelloeffler23@gmail.com>
2023-07-26 21:29:49 -0500

    enables toggle menu feature and replaces menu icon with X when expanded need to work on enabling it only on mobile and having menu tabs extend down (overlay)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,60 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import 'bootstrap';
 
 function Navigation({ currentPage, handlePageChange }) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function toggleMenu() {
+    if (!showMenu) {
+      setShowMenu(true);
+    } else {
+      setShowMenu(false);
+    }
+  }
+
   return (
-    <>
-    <button className='btn' type="button" data-bs-toggle="collapse" data-bs-target="#menuTabs" aria-expanded="false" aria-controls='#menuTabs'>
-    <FontAwesomeIcon icon={faBars} size='2xl'/>
-    </button>
-    
-    <div className='collapse align-items-end' id='menuTabs'>
-     <ul className="nav nav-tab">
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About Me')}
-          className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
-          style={{color: "black"}}
-        >
-          About Me
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-          style={{color: "black"}}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-          style={{color: "black"}}
-        >
-          Resume
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-          style={{color: "black"}}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div className='d-flex justify-content-end'>
+      <div className={
+        showMenu
+          ? 'd-flex align-items-end'
+          : 'd-none'
+      }>
+        <ul className="nav nav-tabs align-items-end">
+          <li className="nav-item">
+            <a
+              href="#about"
+              onClick={() => handlePageChange('About Me')}
+              className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
+              style={{ color: "black" }}
+            >
+              About Me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+              style={{ color: "black" }}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#resume"
+              onClick={() => handlePageChange('Resume')}
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              style={{ color: "black" }}
+            >
+              Resume
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              style={{ color: "black" }}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <button onClick={toggleMenu} type="button" className='btn' aria-label='Hamburger Menu'>
+        {showMenu ? (
+          <FontAwesomeIcon icon={faXmark} size='2xl' />
+        ) : (
+          <FontAwesomeIcon icon={faBars} size='2xl' />
+        )}
+      </button>
     </div>
-    </>
   );
 }
 
